0b01ae7f68613c7fd3335248e881e6429424e0ec
Rahul Chudasama <22010101032@darshan.ac.in>
2025-01-01 10:24:44 +0530

    New

---
+++ index.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class Index{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Number:");
+        int n1=sc.nextInt();
+        sc.close();
+        System.out.println(n1);
+        
+    }
+}
